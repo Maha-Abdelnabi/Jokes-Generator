4150520bc0d3555566853cecebef997b3064c608
Maha Abdelnabi <mahaomar@gmail.com>
2023-09-05 11:29:03 -0400

    get the api key

---
+++ script.js
@@ -0,0 +1,7 @@
+const apiKey = "ojHP7mMMHIPJck9K0WS47A==AU8c2GEESN0bCldo";
+
+const options = {
+  method: "GET",
+  headers: { "X-Api-Key": apiKey }
+};
+ const apiUrl = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
