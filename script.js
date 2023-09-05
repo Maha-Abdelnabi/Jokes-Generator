4e3595da04b4f0b1cc9a98a44c59ca7a19292cba
Maha Abdelnabi <mahaomar@gmail.com>
2023-09-05 11:41:55 -0400

    create a func to get the joke

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
+const btnEl = document.getElementById("btn")
+
+//api
 const apiKey = "ojHP7mMMHIPJck9K0WS47A==AU8c2GEESN0bCldo";
 
-const options = {
+const methods = {
   method: "GET",
-  headers: { "X-Api-Key": apiKey }
+  headers: { "X-Api-Key": apiKey },
 };
  const apiUrl = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
+
+
+ //tell joke function
+ async function tellJoke() {
+    const response =await fetch(apiUrl,methods);
+    const data = await response.json()
+console.log(data)
+}
+//add eventlistener
+btnEl.addEventListener('click', tellJoke);
